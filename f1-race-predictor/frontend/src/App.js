ce581936771db609927caee02e83e31849b0c3fd
Arda Gediz <sc23ag@leeds.ac.uk>
2024-06-15 15:05:48 +0100

    Sq

--- f1-race-predictor/frontend/src/App.js
+++ f1-race-predictor/frontend/src/App.js
@@ -3,8 +3,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Import your existing components
-import HomePage from './HomePage'; // Adjust the import path if needed
-import AnotherPage from './AnotherPage'; // Adjust the import path if needed
+// import HomePage from './HomePage'; // Adjust the import path if needed
+// import AnotherPage from './AnotherPage'; // Adjust the import path if needed
 // Import the new F1Data component
 import F1Data from './F1Data';
 
@@ -13,8 +13,8 @@ function App() {
         <Router>
             <div className="App">
                 <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="/another" component={AnotherPage} />
+                    {/* <Route exact path="/" component={HomePage} /> */}
+                    {/* <Route path="/another" component={AnotherPage} /> */}
                     <Route path="/f1data" component={F1Data} /> {/* New route for F1 data */}
                 </Switch>
             </div>
